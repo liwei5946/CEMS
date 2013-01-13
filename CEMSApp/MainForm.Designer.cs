98f7750f24b8c5b9d2ddd4025b9ee0f8c46e4960
liwei5946 <liwei5946@gmail.com>
2013-01-13 23:28:04 +0800

    再次测试提交

--- CEMSApp/MainForm.Designer.cs
+++ CEMSApp/MainForm.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(375, 67);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(233, 12);
             this.label1.TabIndex = 0;
